1fb1e461f4e209db57aff307b61fd6cd988e2975
vedivero <vedivero@github.com>
2024-07-12 22:44:50 +0900

    Configure the UI screen of the counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css';
-
+import Controller from './component/Controller';
+import Viewer from './component/Viewer';
 function App() {
-  return <>카운터앱</>;
+  return (
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
+  );
 }
 
 export default App;
