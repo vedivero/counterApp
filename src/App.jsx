4b58faf3ff826a1a4591143ce2c3c8b90e01d173
vedivero <vedivero@github.com>
2024-07-17 19:26:40 +0900

    create button module

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import Controller from './component/Controller';
 import Viewer from './component/Viewer';
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+
   return (
     <div className='App'>
       <h1>Simple Counter</h1>
       <section>
-        <Viewer />
+        <Viewer count={count} />
       </section>
       <section>
-        <Controller />
+        <Controller onClickButton={onClickButton} />
       </section>
     </div>
   );
